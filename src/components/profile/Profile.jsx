7222cc97edf945581f4fbfb7358a701dc6c8ec94
Giorgi Bokuchava <g.bokuchava22@gmail.com>
2023-01-05 21:46:11 +0400

    Displayed Missions from API

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
 
 const Profile = () => {
   return (
-    <div>Profile</div>
-  )
-}
+    <div className="container d-flex justify-content-center my-5">
+      <div>
+        <h2>My Missions</h2>
+        <div className="card mission-card">
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">sample 1</li>
+            <li className="list-group-item">sample 2</li>
+            <li className="list-group-item">sample 3</li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <h2 className="ms-5">My Rockets</h2>
+        <div className="card rocket-cart ms-5">
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">sample 1</li>
+            <li className="list-group-item">sample 2</li>
+            <li className="list-group-item">sample 3</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
